d4164555106a02fb07fa441c6cc68d585909f081
Yogesh M <67823621+DigitGhost@users.noreply.github.com>
2021-07-16 11:08:00 +0530

    Update LoginPage.java

--- login/LoginPage.java
+++ login/LoginPage.java
@@ -9,6 +9,7 @@ import javax.swing.JOptionPane;
 import java.awt.SystemColor;
 import java.awt.Font;
 import java.awt.Color;
+
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 import javax.swing.JPasswordField;
@@ -19,7 +20,7 @@ import java.awt.event.ActionEvent;
 public class LoginPage {
 
 	private JFrame frame;
-	private JTextField textField;
+	private JTextField txtUser;
 	private JPasswordField passwordField;
 
 	/**
@@ -52,51 +53,56 @@ public class LoginPage {
 	private void initialize() {
 		frame = new JFrame();
 		frame.getContentPane().setForeground(SystemColor.control);
-		frame.getContentPane().setBackground(Color.DARK_GRAY);
+		frame.getContentPane().setBackground(new Color(0xf3f7f7));
 		frame.setBounds(100, 100, 481, 336);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
-		
+		frame.setTitle("Login");
+				
 		JLabel lblNewLabel = new JLabel("Login");
-		lblNewLabel.setForeground(Color.WHITE);
+		lblNewLabel.setForeground(Color.BLACK);
 		lblNewLabel.setFont(new Font("Cambria Math", Font.BOLD, 40));
 		lblNewLabel.setBackground(Color.WHITE);
-		lblNewLabel.setBounds(167, 10, 125, 43);
+		lblNewLabel.setBounds(171, 10, 125, 43);
 		frame.getContentPane().add(lblNewLabel);
 		
 		JLabel lblNewLabel_1 = new JLabel("Username");
 		lblNewLabel_1.setFont(new Font("Trebuchet MS", Font.PLAIN, 26));
-		lblNewLabel_1.setForeground(Color.LIGHT_GRAY);
+		lblNewLabel_1.setForeground(Color.BLACK);
 		lblNewLabel_1.setBounds(43, 90, 149, 37);
 		frame.getContentPane().add(lblNewLabel_1);
 		
 		JLabel lblNewLabel_2 = new JLabel("Password");
-		lblNewLabel_2.setForeground(Color.LIGHT_GRAY);
+		lblNewLabel_2.setForeground(Color.BLACK);
 		lblNewLabel_2.setFont(new Font("Trebuchet MS", Font.PLAIN, 26));
 		lblNewLabel_2.setBounds(43, 155, 149, 37);
 		frame.getContentPane().add(lblNewLabel_2);
 		
-		textField = new JTextField();
-		textField.setFont(new Font("Times New Roman", Font.PLAIN, 22));
-		textField.setHorizontalAlignment(SwingConstants.LEFT);
-		textField.setBounds(218, 90, 217, 37);
-		frame.getContentPane().add(textField);
-		textField.setColumns(10);
+		txtUser = new JTextField();
+		txtUser.setForeground(Color.BLACK);
+		txtUser.setBackground(new Color(0xF2F2F2));
+		txtUser.setToolTipText("");
+		txtUser.setFont(new Font("Times New Roman", Font.PLAIN, 22));
+		txtUser.setHorizontalAlignment(SwingConstants.LEFT);
+		txtUser.setBounds(218, 90, 217, 37);
+		frame.getContentPane().add(txtUser);
+		txtUser.setColumns(10);
 		
 		passwordField = new JPasswordField();
+		passwordField.setBackground(new Color(0xF2F2F2));
 		passwordField.setFont(new Font("Tahoma", Font.PLAIN, 22));
 		passwordField.setBounds(218, 149, 217, 37);
 		frame.getContentPane().add(passwordField);
 		
 		JButton btnNewButton = new JButton("Login");
 		btnNewButton.setFocusable(false);
-		btnNewButton.setForeground(Color.BLACK);
+		btnNewButton.setForeground(Color.WHITE);
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				if(e.getSource() == btnNewButton 
-						&& !textField.getText().isEmpty() 
+						&& !txtUser.getText().isEmpty() 
 						&& !new String(passwordField.getPassword()).isEmpty()) {
-					String user = textField.getText();
+					String user = txtUser.getText();
 					String pass = new String(passwordField.getPassword());
 					
 					try {
@@ -106,16 +112,17 @@ public class LoginPage {
 					}
 					catch(NumberFormatException ne) {
 						JOptionPane.showMessageDialog(null, "Enter only integers in username", "Username", JOptionPane.INFORMATION_MESSAGE);
-						textField.setText(null);
+						txtUser.setText(null);
 						passwordField.setText(null);
 					}
 					
 				}
 			}
 		});
-		btnNewButton.setBackground(Color.LIGHT_GRAY);
-		btnNewButton.setFont(new Font("Trebuchet MS", Font.PLAIN, 24));
+		btnNewButton.setBackground(new Color(0xba94c));
+		btnNewButton.setFont(new Font("Trebuchet MS", Font.BOLD, 24));
 		btnNewButton.setBounds(217, 214, 114, 43);
 		frame.getContentPane().add(btnNewButton);
+		frame.setLocationRelativeTo(null);
 	}
 }
